ae2b5d1f86a3743b39ffd17c5f626f24c5ff02b3
DinogoByte <prkomeve@gmail.com>
2021-02-24 14:16:09 +0800

    9-12

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,39 +1,18 @@
-// pages/index/index.js
+// pages/logs/logs.js
 Page({
 
   /**
-   * 页面的初始数据（小程序中都会向data要数据
+   * 页面的初始数据
    */
   data: {
-    msg: '初始化数据'
-  },
 
-  handleParent(){
-    console.log('parent');
-  },
-  handleChild(){
-    console.log('child');
-  },
-
-  // 跳转
-  toLogs() {
-    // wx.navigateTo({
-    //   url: '/pages/logs/logs',
-    // })
-    wx.redirectTo({
-      url: '/pages/logs/logs',
-    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onLoad()');
-    setTimeout(() => {this.setData({
-      msg: '修改之后的值'
-    })}, 2000)
-    
+
   },
 
   /**
